a115d56bc44fb7fa91a2e537a7d045427fa31f92
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2018-01-10 01:19:15 -0500

    Create create_self_cert.sh

---
+++ create_self_cert.sh
@@ -0,0 +1,8 @@
+#-------------------------this builds certificate if you have none---------------------------
+#self sign certificate
+openssl genrsa -des3 -out server.key 1024
+openssl req -new -key server.key -out server.csr
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+keytool -import -alias tomcat -keystore /opt/tomcat/.keystore -file server.crt
