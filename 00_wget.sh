654bc88c8c8bdd222b5d5350ba39e36ba3c8d714
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2018-01-16 14:40:33 -0500

    Update 00_wget.sh

--- 00_wget.sh
+++ 00_wget.sh
@@ -1,5 +1,9 @@
 # 2018-01-16 GF
-wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/01_centos.sh
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+cd
 wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/02_fresh.sh
 wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/03_enable_tomcat_ssl.sh
 wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/04_monitoring.sh
