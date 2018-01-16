4d660ef19e20f3a20386c6e9cef8e5bc33663ffe
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2018-01-16 11:30:30 -0500

    Update 01_centos.sh

--- 01_centos.sh
+++ 01_centos.sh
@@ -8,13 +8,10 @@ yum upgrade ; \
 yum install -y htop nano wget curl expat-devel openssl-devel ; \
 yum groupinstall -y "Development Tools" ; \
 systemctl stop firewalld ; systemctl disable firewalld ; \
-echo "centos1.domain.net" > /etc/hostname ; \
-cat /etc/hostname ; \
-echo "wait 20s before reboot" ; sleep 20 ; \
-reboot
+cat /etc/hostname
 #2 java jdk
 #yum install java-1.8.0-openjdk-devel
-yum install java-1.7.0-openjdk-devel
+yum install -y java-1.7.0-openjdk-devel
 #3 tomcat security (avec root/sudo)
 groupadd tomcat ; \
 mkdir /opt ; \
