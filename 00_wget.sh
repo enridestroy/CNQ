74cf04ff359cab6278d45fe3b281f87222bda304
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2018-01-16 14:31:53 -0500

    Create 00_wget.sh

---
+++ 00_wget.sh
@@ -0,0 +1,7 @@
+# 2018-01-16 GF
+wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/01_centos.sh
+wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/02_fresh.sh
+wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/03_enable_tomcat_ssl.sh
+wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/04_monitoring.sh
+wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/05_init.sh
+wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/server.xml.new
