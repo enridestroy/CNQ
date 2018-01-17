ee028aaf89358402ad9d50152b5b5d0190315157
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2018-01-17 10:15:20 -0500

    Update 01_wget.sh

--- 01_wget.sh
+++ 01_wget.sh
@@ -3,6 +3,8 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
+vJBOSSCORE=core.jboss.tt.cdnq.lan
+vCUSTOMLOGIN=https://authentification.tt.cdnq.lan
 cd
 wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/02_fresh.sh
 wget https://raw.githubusercontent.com/guyfrancoeur/CNQ/master/03_enable_tomcat_ssl.sh
